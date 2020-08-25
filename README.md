ac80a554e87a93fd3db999bff2d80a98c5fe2fae
Brian Toney <btoney1988@gmail.com>
2020-08-25 15:17:36 -0400

    Major java adjustments

--- README.md
+++ README.md
@@ -1,35 +1,20 @@
 # password_generator
 
-# 03 JavaScript: Password Generator
+##
+ - This is a random password generator. When you click on the Generate Password button, you are shown various prompts that question the criteria for the password being generated. The first prompt is character length between 8-128 (if you pick outside of the number you are presented with an alert.). Then you are asked to confirm the various characters that will go into the password. When finished with the prompts the password displays in the text box shown.
+ - I got some assistance from the youtube channel Traversy Madia ('https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA'). Along with https://www.w3schools.com/
+ - Charset codes thanks to 'http://www.net-comber.com/charset.html'
 
-Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+ # Screenshots
+  - First Prompt
+    ![image](https://user-images.githubusercontent.com/68873509/91215219-35eddd80-e6e2-11ea-872b-071cd8f428a0.png)
+  - Error Alert
+    ![image](https://user-images.githubusercontent.com/68873509/91215390-7188a780-e6e2-11ea-97b9-c8af05dfb3e2.png)
+  - Criteria Confirms
+    ![image](https://user-images.githubusercontent.com/68873509/91215491-95e48400-e6e2-11ea-9279-ed10180eda07.png)
+    ![image](https://user-images.githubusercontent.com/68873509/91215535-a432a000-e6e2-11ea-9a3a-e5cde8ce2e3c.png)
+    ![image](https://user-images.githubusercontent.com/68873509/91215570-b280bc00-e6e2-11ea-8014-49b1ae157fb1.png)
+    ![image](https://user-images.githubusercontent.com/68873509/91215608-be6c7e00-e6e2-11ea-9e7a-fcce649e39cd.png)
+  - Final Password
+    ![image](https://user-images.githubusercontent.com/68873509/91215639-c9271300-e6e2-11ea-919c-4714bd942344.png)
 
-If you are unfamiliar with special characters, take a look at [some examples](https://www.owasp.org/index.php/Password_special_characters).
-
-## User Story
-
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
-
-## Acceptance Criteria
-
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
--- script.js
+++ script.js
@@ -7,74 +7,88 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+
+  // Generator function
   function generatePassword() {
-    var passwordCriteria = ["characterLength, upper, lower, numbers, symbols"];
-    passwordCriteria[0] = prompt("Please write the password length (8-128) for the password.");
 
-    // Random functions for password criteria
-    if (parseFloat(passwordCriteria[0]) >= 8 && parseFloat(passwordCriteria[0]) <= 128) {
-      passwordCriteria[1] = confirm("Uppercase?");
-      passwordCriteria[2] = confirm("Lowercase?");
-      passwordCriteria[3] = confirm("Numbers?");
-      passwordCriteria[4] = confirm("Symbols?");
+    // Final password to be displayed
+    var finalPassword = ''
+    // Prompts and Confirms for password criteria
+    var length = prompt("Please write the password length (8-128) for the password.")
+    // If outside of 8-128 alert will be presented
+    if (parseFloat(length) <= 128 && parseFloat(length) >= 8) {
 
-
-      if (passwordCriteria[1] == true) {
-
-        function getRandomUpper() {
-          return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-
-        };
-
-        console.log(getRandomUpper());
-
-      };
-
-      if (passwordCriteria[2] == true) {
-
-        function getRandomLower() {
-          return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-
-        };
-
-
-        console.log(getRandomLower());
-      };
-
-      if (passwordCriteria[3] == true) {
-
-        function getRandomNumber() {
-          return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-
-        };
-
-        console.log(getRandomNumber());
-
-      };
-
-      if (passwordCriteria[4] == true) {
-
-        function getRandomSymbol() {
-          var symbols = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
-          return symbols[Math.floor(Math.random() * symbols.length)];
-
-        };
-
-        console.log(getRandomSymbol());
-
-      };
-
+      var upper = confirm("Uppercase?");
+      var lower = confirm("Lowercase?")
+      var number = confirm("Numbers?");
+      var symbol = confirm("Symbols?");
     } else {
 
-      alert("start over")
+      alert("Please choose numbers between 8 - 128.");
+    }
+
+    // Based off user input chooses which criteria to use
+    const typesCount = upper + lower + number + symbol;
+
+    console.log('typesCount: ', typesCount);
+
+    // The array will filter out objects what were not confirmed during the prompts
+    const typesArr = [{ upper }, { lower }, { number }, { symbol }].filter(item => Object.values(item)[0]);
+
+    console.log('typesArr: ', typesArr);
+
+    // If no criteria is confirmed then it will return nothing
+    if (typesCount === 0) {
+      return ('');
     };
+    // For loop based off user input of length and criteria
+    for (let i = 0; i < length; i += typesCount) {
+      // Based off user input the criteria is set run the randomize functions 
+      typesArr.forEach(type => {
 
+        const funcName = Object.keys(type)[0];
 
+        console.log('funcName: ' + funcName);
+
+        const criteria = {
+          upper: getRandomUpper(),
+          lower: getRandomLower(),
+          number: getRandomNumber(),
+          symbol: getRandomSymbol(),
+        };
+
+        // Final password is running the 
+        finalPassword += criteria[funcName];
+      });
+
+      console.log(finalPassword)
+
+    }
+
+    // Returning the final product to be placed in the text box.
+    return finalPassword;
   };
 
   passwordText.value = password;
+};
 
-}
+// Functions for random criteria - 'http://www.net-comber.com/charset.html'
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+};
+
+function getRandomSymbol() {
+  var symbols = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
