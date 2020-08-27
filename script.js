e9b5c56e8b5f2abf7bab71f0eac6a51357d2bfc7
Brian Toney <btoney1988@gmail.com>
2020-08-27 10:58:16 -0400

    Java validator adjustments

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 // Generator function
 function generatePassword(characterLength, upper, lower, number, symbol) {
   // Final password set as a string
-  let finalPassword = '';
+  var finalPassword = '';
   // Based off user input chooses which criteria to use
   var criteriaCount = upper + lower + number + symbol;
   // // The array will filter out objects what were not confirmed during the prompts
@@ -11,11 +11,11 @@ function generatePassword(characterLength, upper, lower, number, symbol) {
   // If no criteria is confirmed then it will return nothing
   if (criteriaCount === 0) {
     return ('No criteria given');
-  };
+  }
   // For loop based off user input of length and criteria
-  for (let i = 0; i <= characterLength; i++) {
+  for (var i = 0; i <= characterLength; i++) {
 
-    const r = generater(0, 3);
+    var r = generater(0, 3);
     if (upper && r === 0) {
       finalPassword += getRandomUpper();
     } else if (lower && r === 1) {
@@ -27,12 +27,12 @@ function generatePassword(characterLength, upper, lower, number, symbol) {
     } else {
       i--;
     }
-  };
-  generatedPassword = finalPassword.slice(0, characterLength);
+  }
+  var generatedPassword = finalPassword.slice(0, characterLength);
 
   console.log(generatedPassword);
   return generatedPassword;
-};
+}
 // Write password to the #password input
 function writePassword() {
   // Prompts and Confirms for password criteria
@@ -57,31 +57,29 @@ function writePassword() {
 
   console.log(password);
   passwordText.value = password;
-};
-
-
+}
 
 // Functions for random criteria - 'http://www.net-comber.com/charset.html'
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-};
+}
 
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-};
+}
 
 function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-};
+}
 
 function getRandomSymbol() {
   var symbols = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
   return symbols[Math.floor(Math.random() * symbols.length)];
-};
+}
 
 function generater(min = 0, max = 1) {
   return Math.floor(Math.random() * (max + 1 - min) + min);
-};
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
